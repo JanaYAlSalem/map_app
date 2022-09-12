84c573a3d1c99da67652e7dabdc6096a8500c75c
Jana <JanaAlSalem@outlook.sa>
2022-09-12 13:15:48 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'ui/screens/map_screens.dart';
+
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
+}
+
